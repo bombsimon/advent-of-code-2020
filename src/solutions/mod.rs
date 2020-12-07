51904e322436747d7a96056ed942f9dcd3f0d3d2
Simon Sawert <simon@sawert.se>
2020-12-07 23:37:43 +0100

    Combine day 5
    Use svn to see improvements made to the solution instead of keeping
    track of multiple implementations

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -3,7 +3,6 @@ mod day02;
 mod day03;
 mod day04;
 mod day05;
-mod day05_alt;
 mod day06;
 mod day07;
 
@@ -15,13 +14,7 @@ pub fn solution_for(day: i32) {
         2 => day02::solve(),
         3 => day03::solve(),
         4 => day04::solve(),
-        5 => {
-            println!("Solution #1");
-            day05::solve();
-
-            println!("\nSolution #2");
-            day05_alt::solve();
-        }
+        5 => day05::solve(),
         6 => day06::solve(),
         7 => day07::solve(),
         d => panic!(format!("Day {} not implemented", d)),
